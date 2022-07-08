672304de795fe351f75e8e68f3069771656790de
Mykhaylo Shumko <msshumko@gmail.com>
2022-07-08 17:33:42 -0400

    Writing the L-MLT map loop.

--- sampex_maps/calc_l_mlt_map.py
+++ sampex_maps/calc_l_mlt_map.py
@@ -1,30 +1,82 @@
-# Calculate L-MLT map of SAMPEX-HILT (>1 MeV electron) precipitation.
-
 from datetime import datetime, timedelta
+import pathlib
+import re
 
+import pandas as pd
 import numpy as np
 import sampex
 
 from sampex_maps.utils import progressbar
 
 class L_MLT_Map:
-    def __init__(self, L_bins, MLT_bins, times=None) -> None:
+    def __init__(self, L_bins, MLT_bins, instrument='HILT', times=None, stat=50) -> None:
+        """
+        Calculate L-MLT map of SAMPEX precipitation. Once you run L_MLT_Map.loop(), the 
+        "H" attribute contains the histogram.
+
+        Parameters
+        ----------
+        L_bins: np.array
+            The L bins used to histogram the data.
+        MLT_bins: np.array
+            The MLT bins used to histogram the data.
+        instrument: str
+            The SAMPEX instrument. Can be 'HILT', 'PET', or 'LICA'.
+        times: np.array
+            An shape = (n, 2) array of start and end times. Not implemented yet.
+        stat: float
+            What percentile to calculate. Default is the median.
+        """
         self.H = np.zeros((L_bins.shape[0]-1, MLT_bins.shape[0]-1))
+        self.instrument = instrument.upper()
+        assert instrument.upper() in ['HILT', 'PET', 'LICA']
 
         if times is not None:
             # TODO use the times kwarg to filter the times over which
             # the loop loops over.
             raise NotImplementedError
         else:
-            self.dates = None
+            self.dates = self._get_dates()
         return
 
-    def loop():
-        pass
+    def loop(self):
+        """
+        Loop over the SAMPEX files and bin each day's data by L and MLT.
+        """
+        for date in progressbar(self.dates):
+            self.hilt = sampex.HILT(date)
+            self.attitude = sampex.Attitude(date)
 
+    def _yeardoy2date(self, yeardoy):
+        """
+        Converts a date in the year day-of-year format (YEARDOY)
+        into a datetime.datetime object.
+        """
+        return datetime.strptime(yeardoy, "%Y%j")
+
+    def _get_dates(self):
+        """
+        Finds the SAMPEX files and converts the filenames into dates.
+        """
+        if self.instrument.upper() == 'HILT':
+            # Find the files and convert the YYYYDOY component of the filename 
+            # to a datetime.
+            file_name_glob = f"hhrr*"
+            self.hilt_paths = sorted(
+                pathlib.Path(sampex.config['sampex_data_dir']).rglob(file_name_glob))
+            assert len(self.hilt_paths), (
+                f'{len(self.hilt_paths)} HILT files found in '
+                f'{sampex.config["sampex_data_dir"]}'
+                )
+            date_strs = [re.findall(r"\d+", str(f.name))[0] for f in self.hilt_paths]
+            dates = [self._yeardoy2date(date_str) for date_str in date_strs]
+        else:
+            raise NotImplementedError
+        return dates
 
 if __name__ == '__main__':
     L_bins = np.arange(2, 11)
     MLT_bins = np.arange(0, 24.1)
 
     m = L_MLT_Map(L_bins, MLT_bins)
+    m.loop()
---
+++ sampex_maps/dial.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Dial:
+    def __init__(self, ax, angular_bins, radial_bins, H):
+        """ 
+        This class makes a dial (polar) plot were MLT is the azimuthal
+        coordinate and L shell is the radial coordinate. 
+        """
+        self.ax = ax
+        self.angular_bins = angular_bins
+        self.radial_bins = radial_bins
+        self.H = H
+
+        if 'Polar' not in str(type(ax)):
+            raise ValueError('Subplot is not polar. For example, '
+                'create ax with \n ax[0] = plt.subplot(121, projection="polar")')
+        return
+
+    def draw_dial(self, colorbar=True, L_labels=[2,4,6,8], mesh_kwargs={}, colorbar_kwargs={}):
+        """
+        Draws a dial plot on the self.ax subplot object (must have projection='polar' kwarg). 
+        colorbar=True - Plot the colorbar or not.
+        L_labels=[2,4,6,8] - What L labels to plot
+        mesh_kwargs={} - The dictionary of kwargs passed to plt.pcolormesh() 
+        colorbar_kwargs={} - The dictionary of kwargs passed into plt.colorbar()
+        """
+        self.L_labels = L_labels
+
+        angular_grid, radial_grid = np.meshgrid(self.angular_bins, self.radial_bins)
+
+        # Try-except block deals with the dimensions of the mesh and taransposes it
+        # if necessary.
+        try:
+            p = self.ax.pcolormesh(angular_grid*np.pi/12, radial_grid, self.H.T, **mesh_kwargs)
+        except TypeError as err:
+            if 'Dimensions of C' in str(err):
+                p = self.ax.pcolormesh(angular_grid*np.pi/12, radial_grid, self.H, **mesh_kwargs)
+            else:
+                raise
+
+        self.draw_earth()
+        self._plot_params()
+
+        if colorbar:
+            plt.colorbar(p, ax=self.ax, **colorbar_kwargs)
+        return
+
+    def draw_earth(self, earth_resolution=50):
+        """ Given a subplot object, draws the Earth with a shadow"""
+        # Just x,y coords for a line (to map to polar coords)
+        earth_circ = (np.linspace(0, 2*np.pi, earth_resolution), np.ones(earth_resolution)) 
+        # x, y_lower, y_upper coords for Earth's shadow (maps to polar).
+        earth_shadow = (
+                        np.linspace(-np.pi/2, np.pi/2, earth_resolution), 
+                        0, 
+                        np.ones(earth_resolution)
+                        )
+        self.ax.plot(*earth_circ, c='k')
+        self.ax.fill_between(*earth_shadow, color='k')
+        return
+
+    def _plot_params(self):
+        # Draw L shell contours and get L and MLT labels 
+        L_labels_names = self._draw_L_contours()
+        mlt_labels = (self.ax.get_xticks()*12/np.pi).astype(int)
+        # Sun facing up.
+        self.ax.set_xlabel('MLT')
+        self.ax.set_theta_zero_location("S") # Midnight at bottom
+        self.ax.set_xticklabels(mlt_labels) # Transform back from 0->2pi to 0->24.
+        self.ax.set_yticks(self.L_labels)
+        self.ax.set_yticklabels(L_labels_names, fontdict={'horizontalalignment':'right'})
+        return
+
+    def _draw_L_contours(self, earth_resolution=50):
+        """ Plots a subset of the L shell contours. """
+        # Draw azimuthal lines for a subset of L shells.
+        L_labels_names = [str(i) for i in self.L_labels[:-1]] + [f'L = {self.L_labels[-1]}']
+        # L_labels_names = [str(i) for i in self.L_labels]
+        for L in self.L_labels:
+            self.ax.plot(np.linspace(0, 2*np.pi, earth_resolution), 
+                        L*np.ones(earth_resolution), ls=':', c='k')
+        return L_labels_names
